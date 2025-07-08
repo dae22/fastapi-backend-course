227a785ff4a2380929d97e349d84a27338979636
dae22 <dae-22@mail.ru>
2025-07-08 18:52:20 +0300

    Make CRUD endpoints

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -2,18 +2,37 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+tasks = dict()
+next_id = 0
+
+
 @app.get("/tasks")
 def get_tasks():
-    pass
+    return {'List of tasks': tasks}
+
 
 @app.post("/tasks")
 def create_task(task):
-    pass
+    global next_id
+    new_task = {'task': task, 'status': False}
+    tasks[next_id] = new_task
+    next_id += 1
+    return {'message': f'New task added. Assigned id: {next_id - 1}'}
+
 
 @app.put("/tasks/{task_id}")
 def update_task(task_id: int):
-    pass
+    if task_id in tasks.keys():
+        tasks[task_id]['status'] = True
+        return {'message': 'Task marked as completed'}
+    else:
+        return {'message': 'Task not found'}
+
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    if task_id in tasks.keys():
+        del tasks[task_id]
+        return {'message': 'Task deleted'}
+    else:
+        return {'message': 'Task not found'}
